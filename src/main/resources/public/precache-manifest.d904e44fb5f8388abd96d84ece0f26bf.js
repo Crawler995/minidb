0bb669d2c0b4837066db99f04d1c50e1dfb03921
crawler995 <zhang_995@foxmail.com>
2020-09-30 17:35:07 +0800

    feat: 将前端集成到后端

---
+++ src/main/resources/public/precache-manifest.d904e44fb5f8388abd96d84ece0f26bf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0bbe957c1cf1378b654e3fe4126ac89",
+    "url": "./index.html"
+  },
+  {
+    "revision": "65193e17437964135701",
+    "url": "./static/css/2.99a5358a.chunk.css"
+  },
+  {
+    "revision": "411c651f47fd4d99b1b7",
+    "url": "./static/css/main.9088832d.chunk.css"
+  },
+  {
+    "revision": "65193e17437964135701",
+    "url": "./static/js/2.49e58b9d.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "./static/js/2.49e58b9d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "411c651f47fd4d99b1b7",
+    "url": "./static/js/main.511888ee.chunk.js"
+  },
+  {
+    "revision": "87941d29ab64a1fff3df",
+    "url": "./static/js/runtime-main.6a1fbeb8.js"
+  }
+]);
